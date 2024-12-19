717491dd8dd516462f10b396f75358033e59b23e
Unmesh S Pimple <unmeshp55@gmail.com>
2024-12-19 23:25:42 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-import logger
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -20,9 +19,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-# if __name__ == '__main__':
-#     try:
-#         1/0
-#     except Exception as e:
-#         logging.info('Divided by Zero')
-#         raise CustomException(e,sys)
+if __name__ == '__main__':
+    try:
+        1/0
+    except Exception as e:
+        logging.info('Divided by Zero')
+        raise CustomException(e,sys)
